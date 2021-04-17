120fe5ea0252e5b9b62154ac24e4c82633a67faa
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-04-17 16:33:25 +0530

    feat: add todolist component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  const todos = [
+    {
+      id: 1,
+      text: "this is first task",
+    },
+    {
+      id: 2,
+      text: "this is second task",
+    },
+  ];
+
+  return (
+    <div className="App">
+      <TodoList items={todos} />
+    </div>
+  );
 };
 
 export default App;
